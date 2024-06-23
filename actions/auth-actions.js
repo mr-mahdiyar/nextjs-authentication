76248e09b3592926ad773aeddad4414e85482610
Mahdiyar <m.poursalari3200@gmail.com>
2024-06-23 11:51:18 +0330

    Authentication server form action was added.

---
+++ actions/auth-actions.js
@@ -0,0 +1,16 @@
+"use server";
+export async function signup(prevState, formData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+
+  let errors = {};
+  if (!email.includes("@")) {
+    errors.email = "please enter a valid email address.";
+  }
+  if (password.trim().length < 8) {
+    errors.password = "Password must be atleast 8 characters long.";
+  }
+  if (Object.keys(errors).length > 0) {
+    return { errors };
+  }
+}
